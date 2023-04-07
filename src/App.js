ae9018b4e4813dd47af94edde27ebd6e4ea96a2f
Valeria Daffarra <100484756+Valeduff@users.noreply.github.com>
2023-04-07 15:31:36 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState }  from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+
+
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  const addContacts = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
+
+  const addAppointments = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
+
+
+  return (
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={ROUTES.CONTACTS} />
+          </Route>
+          <Route path={ROUTES.CONTACTS}>
+             {/* Add props to ContactsPage */}
+            <ContactsPage 
+            addContacts={addContacts}
+            contacts={contacts} />
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            {/* Add props to AppointmentsPage */}
+            <AppointmentsPage 
+            addAppointments={addAppointments}
+            appointments={appointments}
+            contacts={contacts} />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
